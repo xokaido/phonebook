eea107c2d3c7cfcb4117d556fd03bc39e5a98292
= <xokaido@gmail.com>
2018-02-12 06:29:01 +0400

    Improved the arcitecture ( moved model functions to the appropriate place ), fixed security issues (non-logged-in users could "delete" records )

--- ci/app/controllers/Phonebooks.php
+++ ci/app/controllers/Phonebooks.php
@@ -3,62 +3,49 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Phonebooks extends CI_Controller {
 
+  private $user;  // Store a logged in user here
+  public function __construct()
+  {
+      parent::__construct();
+      if ( ! $this->ion_auth->logged_in() )
+        redirect('auth/register');
+
+      $this->user = $this->ion_auth->user()->row();
+      $this->load->model( 'phonebook_model' );
+
+  }
+
 	public function index()
 	{
-
-      $user = $this->ion_auth->user()->row();
       $this->load->library('Datatables');
 
       echo $this->datatables
                 ->select( "id, name, phone, notes, created_on" )
                 ->from( "phonebooks")
-                ->where('user_id', $user->id )
+                ->where('user_id', $this->user->id )
                 ->generate();
 
 	}
   public function add()
   {
-    list( $id, $name, $phone, $created_on, $notes ) = $this->input->post( 'data' );
-    $datetime = date( 'Y-m-d H:i:s' );
-    $data = [
-              'user_id'     => $this->ion_auth->user()->row()->id,
-              'name'        => ( !empty( $name ) ? $name : 'n/a' ), 
-              'phone'       => ( !empty( $phone ) ? $phone : 'n/a' ),
-              'created_on'  => ( !empty( $created_on ) ? $created_on : $datetime ),
-              'updated_on'  => $datetime,
-              'notes'       => ( !empty( $notes ) ? $notes : 'n/a' )
-    ];
-    if( $this->db->insert( 'phonebooks', $data ) )
+    if( $this->phonebook_model->add() )
       echo 'Success!';
     else
       echo 'Failure!';
-
   }
   public function edit()
   {
-    list( $id, $name, $phone, $created_on, $notes ) = $this->input->post( 'data' );
-    
-    $data = [ 'id'          => $id,
-              'user_id'     => $this->ion_auth->user()->row()->id,
-              'name'        => $name, 
-              'phone'       => $phone, 
-              'created_on'  => $created_on,
-              'notes'       => $notes
-    ];
-    if( $this->db->replace( 'phonebooks', $data ) )
+    if( $this->phonebook_model->edit() )
       echo 'Success!';
     else
       echo 'Failure!';
-
   }
   public function delete()
   {
-    list( $id, $name, $phone, $created_on, $notes ) = $this->input->post( 'data' );
-    $this->db->where( 'id', $id );
-    if( $this->db->delete([ 'phonebooks' ]) )
+    if( $this->phonebook_model->delete( ) )
       echo 'Success!';
     else
       echo 'Failure!';
-
   }
+
 }
